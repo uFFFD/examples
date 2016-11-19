526e3bda9e995fb44ebd5e7c04d83b15d6468ad6
uFFFD <xefbfbd@gmail.com>
2016-11-19 10:50:07 +0800

    [flyfile] refactor serveFileDownload()

--- flyfile/flyfile.js
+++ flyfile/flyfile.js
@@ -143,75 +143,69 @@ function listFiles () {
 }
 
 function serveFileDownload (req) {
-  var id = Number.parseInt(req.url.split("/")[2]);
-  var files = get_shared_files();
-  var status;
-  var headers = new Headers();
-  var body;
-  var msg = [];
+  const id = Number.parseInt(req.url.split("/")[2]);
+  const files = get_shared_files();
 
-  if (id >= 0 && id < files.length) {
-    var file = files[id];
-    var filesize = file.size;
-    var type = file.type || "binary/octet-stream";
-
-    headers.set("Content-Type", type);
-    headers.set("Content-Disposition",
-                "inline; filename*=UTF-8''" +
-                encodeRFC5987ValueChars(file.name));
-    headers.set("Accept-Ranges", "bytes");
-
-    msg.push("Sending file " + file.name);
-
-    if (req.headers.has("range")) {
-      var range = parseRangeHeader(req.headers.get("range"), filesize);
-      status = range.status;
-
-      if (status == 416) {
-        msg.push("416 Requested Range Not Satisfiable");
-        headers.set("Content-Range", "bytes */" + filesize);
-        body = "416 Requested Range Not Satisfiable";
-      } else if (status == 206) {
-        var satisfiable;
-        if (range.multipart) {
-          satisfiable = range.satisfiable;
-          var boundary = Date.now();
-          msg.push("multipart/byteranges: " + satisfiable.map(function (el) {
-            return el.map(prettyPrintSize).join("-");
-          }).join(","));
-          headers.set("Content-Type",
-                      "multipart/byteranges; boundary=" + boundary);
-          var CRLF = "\r\n";
-          var parts = [];
-          for (var el of satisfiable) {
-            parts.push(CRLF + "--" + boundary + CRLF);
-            parts.push("Content-type: " + type + CRLF);
-            parts.push("Content-range: bytes " +
-                       el.join("-") + "/" + filesize + CRLF);
-            parts.push(CRLF);
-            parts.push(file.slice(el[0], el[1] + 1));
-          }
-          parts.push(CRLF + "--" + boundary + "--" + CRLF);
-          body = new Blob(parts);
-        } else {
-          satisfiable = range.satisfiable[0];
-          msg.push("range: " + satisfiable.map(prettyPrintSize).join("-"));
-          headers.set("Content-Range",
-                      "bytes " + satisfiable.join("-") + "/" + filesize);
-          body = file.slice(satisfiable[0], satisfiable[1] + 1);
-        }
-      } else {
-        body = file;
-      }
-    } else {
-      status = 200;
-      body = file;
-    }
-  } else {
+  if (id < 0 || id >= files.length) {
     return Promise.reject(new HTTPError(`Requested file index out of bounds: ${id}`, {
       status: 404,
       body: "404 Not Found",
     }));
+  }
+
+  let headers = {};
+  let msg = [];
+  const file = files[id];
+  const type = file.type || "binary/octet-stream";
+
+  headers["Content-Type"] = type;
+  headers["Content-Disposition"] = `inline; filename*=UTF-8''${encodeRFC5987ValueChars(file.name)}`;
+  headers["Accept-Ranges"] = "bytes";
+
+  msg.push(`Sending file ${file.name}`);
+
+  if (!req.headers.has("range")) {
+    return Promise.resolve(new Response(file, {headers: headers}));
+  }
+
+  const filesize = file.size;
+  const range = parseRangeHeader(req.headers.get("range"), filesize);
+  let status = range.status;
+  let body;
+
+  if (status == 200) {
+    body = file;
+  } else if (status == 416) {
+    msg.push("416 Requested Range Not Satisfiable");
+    headers["Content-Range"] = `bytes */${filesize}`;
+    body = "416 Requested Range Not Satisfiable";
+  } else if (status == 206) {
+    if (!range.multipart) {
+      let satisfiable = range.satisfiable[0];
+      msg.push(`range: ${satisfiable.map(prettyPrintSize).join("-")}`);
+      headers["Content-Range"] = `bytes ${satisfiable.join("-")}/${filesize}`;
+      body = file.slice(satisfiable[0], satisfiable[1] + 1);
+    } else {
+      let satisfiable = range.satisfiable;
+      const boundary = Date.now();
+      msg.push(`multipart/byteranges: ${satisfiable.
+        map(e => e.map(prettyPrintSize).join("-")).
+        join(",")}`);
+      headers["Content-Type"] = `multipart/byteranges; boundary=${boundary}`;
+      let parts = [];
+      satisfiable.forEach(e => {
+        parts.push(`\r\n--${boundary}\r\n` +
+                   `Content-type: ${type}\r\n` +
+                   `Content-range: bytes ${e.join("-")}/${filesize}\r\n\r\n`);
+        parts.push(file.slice(e[0], e[1] + 1));
+      });
+      parts.push(`\r\n--${boundary}--\r\n`);
+      body = new Blob(parts);
+    }
+  } else {
+    status = 500;
+    body = "500 Internal Server Error";
+    headers = {};
   }
 
   LOG(msg.join(", "));
