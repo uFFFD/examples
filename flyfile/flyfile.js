d42db467898b9abf55c7043c8973099acab74457
uFFFD <xefbfbd@gmail.com>
2016-11-19 12:49:21 +0800

    [flyfile] satisfy jshint

--- flyfile/flyfile.js
+++ flyfile/flyfile.js
@@ -163,7 +163,7 @@ function serveFileDownload (req) {
   headers["Accept-Ranges"] = "bytes";
 
   return sha256(`${file.name}\n${file.size}\n${file.lastModified}`).then(etag => {
-    headers["ETag"] = `"${etag}"`;
+    headers.ETag = `"${etag}"`;
 
     msg.push(`Sending file ${file.name}`);
 
@@ -372,12 +372,12 @@ function hex(buffer) {
   var view = new DataView(buffer);
   for (var i = 0; i < view.byteLength; i += 4) {
     // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-    var value = view.getUint32(i)
+    var value = view.getUint32(i);
     // toString(16) will give the hex representation of the number without padding
-    var stringValue = value.toString(16)
+    var stringValue = value.toString(16);
     // We use concatenation and slice for padding
-    var padding = '00000000'
-    var paddedValue = (padding + stringValue).slice(-padding.length)
+    var padding = '00000000';
+    var paddedValue = (padding + stringValue).slice(-padding.length);
     hexCodes.push(paddedValue);
   }
 
